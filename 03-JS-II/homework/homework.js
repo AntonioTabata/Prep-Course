df044060be8ac32fd9b1c01b26fc881b7f56dc29
Antonio <adtc22@gmail.com>
2022-12-16 07:59:07 -0400

    yanopuedo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -191,7 +191,7 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
  for (var i=2; i<numero;i++){
   if(numero%i === 0){
-    return false;
+    return false
   }
   return true
  }
