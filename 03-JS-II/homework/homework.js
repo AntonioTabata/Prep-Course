b96528a068c3af3e5501c48ea3b79337e607267b
Antonio <adtc22@gmail.com>
2022-12-15 22:35:11 -0400

    una vez mas

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -131,10 +131,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if (numero = Math.trunc(numero)){
+  if (numero === Math.trunc(numero)){
     return true
   }
-  return flase
+  return false
 }
 
 function fizzBuzz(numero) {
@@ -163,22 +163,25 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if(num1 > 0 && num1 > num2 && num1 > num3 ){
-    return 'Número 1 es mayor y positivo'
-  }
-  else if(num1 <0 || num2 <0||num3<0){
+  if(num1 <0 || num2 <0 || num3<0){
     return 'Hay negativos';
+  }
+  else if(num1 === 0 || num2 === 0||num3===0){
+    'Error'
   }
   else if (num3>num1 && num3 > num2){
     return num3+1
   }
-  else if(num1 ===0 || num2 ===0||num3===0){
-    'Error'
+  else if(num1 > 0 && num1 > num2 && num1 > num3 ){
+    return 'Número 1 es mayor y positivo'
   }
   else{
     return false
   }
 }
+
+
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
