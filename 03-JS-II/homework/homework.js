56235b4503cdd34c53e72fdaf9e64edfef1af79f
Antonio <adtc22@gmail.com>
2022-12-16 07:25:14 -0400

    primocorrecion

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -190,12 +190,11 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   for(var i=2; i<numero;i++){
-    if ((numero % i)  === 0) {
-      return false;
-    }
-    else{
+    if ((numero % i)  !== 0) {
       return true;
     }
+      return false;
+    
   }
 
 }
