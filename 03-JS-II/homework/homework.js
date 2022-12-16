a23e1d4618ff42ef5c29735f7a869291c3f79b5b
Antonio <adtc22@gmail.com>
2022-12-16 07:38:51 -0400

    me aburre esprimo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -189,15 +189,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  for(var i=2; i<numero;i++){
-    if ((numero % i)  !== 0) {
-      return true;
-    }
-    else ((numero % i)  === 0)
-      return false;
-    
+ for (var i=2; i<numero;i++){
+  if(numero%i ===0){
+    return false;
   }
-
+  else{
+    return true
+  }
+ }
 }
 
 function esVerdadero(valor){
