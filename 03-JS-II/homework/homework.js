3d40cd6297441761334d3437b17bae710b090b07
Antonio <adtc22@gmail.com>
2022-12-19 11:26:41 -0400

    tabla del seis

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -193,8 +193,6 @@ if (numero <2) return false;
 if (numero === 2) return false;
 for (var i=2 ; i < numero; i++)  {
   if (numero % i === 0) return false;
-
-  
 }
 return true;
 }
@@ -216,8 +214,12 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-
-  
+var tabla = []
+for (var i=0; i<=10; i++){
+  var resultadoproducto = 6 * i;
+  tabla.push(6*i);
+}
+  return tabla
 }
 
 function tieneTresDigitos(numero){
