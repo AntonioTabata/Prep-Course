74fe7c2ca4f3fa1e054fb5941c996766fbd5a811
Antonio <adtc22@gmail.com>
2022-12-19 10:51:32 -0400

    prueba variacion esprimo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -191,11 +191,15 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 if (numero <2) return false;
 if (numero === 2) return false;
-for (var i=2 ; i <= numero - 1 ; i++)  {
-  if (numero % i === 0) return false;
-
+for (var i=2 ; i < numero; i++)  {
+  if (numero % i === 0) {
+    return false;
+  } 
+  else{
+    return true;
+  }
 }
-return true;
+
 }
 
 function esVerdadero(valor){
